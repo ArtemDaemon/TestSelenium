6fcb52f3c219d32f5dbc44c566bc9bc71e247978
ArtemDaemon <artem.daemon.official@gmail.com>
2025-04-17 15:31:27 +0700

    Добавил принятие соглашения и скрол до элемента

--- pages/HomePage.py
+++ pages/HomePage.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 class HomePage:
@@ -12,8 +13,24 @@ class HomePage:
 
     def go_to_section(self, name: str):
         old_url = self.driver.current_url
-        self.driver.find_element(By.LINK_TEXT, name).click()
+        element = self.driver.find_element(By.LINK_TEXT, name)
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
+        element.click()
         WebDriverWait(self.driver, 5).until(EC.url_changes(old_url))
+
+    def accept_cookies(self):
+        try:
+            wait = WebDriverWait(self.driver, 5)
+            button = wait.until(
+                EC.element_to_be_clickable((By.CLASS_NAME, "biscuit-button"))
+            )
+            button.click()
+
+            wait.until(
+                EC.invisibility_of_element_located((By.CLASS_NAME, "biscuit"))
+            )
+        except TimeoutException:
+            pass  # баннер не появился — ок
 
     def get_url(self):
         return self.driver.current_url
