ca0694195fefb70f5ab90adef18ccc668b10ae53
ArtemDaemon <artem.daemon.official@gmail.com>
2025-04-17 12:21:53 +0700

    Установил настройки драйвера Chrom

--- conftest.py
+++ conftest.py
@@ -1,10 +1,14 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
